67dff6a302e680e6d1aa9ff213757c459e7b4058
vpotphod <vpotphod@LIN25005190.corp.capgemini.com>
2021-08-16 15:49:07 +0530

    Commit All

--- firstdemo/src/main/java/firstdemo/HelloWorld.java
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -5,7 +5,16 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-
+		int[] arr = {23,45,56,78,6};
+		for(int num:arr)
+			System.out.println(num);
+		
 	}
-
+	public void  print() {
+		System.out.println("Print all array numbers:");
+	
+	}
 }
+
+
+
