d88035047b03c78224f693031e6e57256efa467c
vpotphod <vpotphod@LIN25005190.corp.capgemini.com>
2021-08-16 18:01:30 +0530

    SalesClass commit

---
+++ firstdemo/src/main/java/firstdemo/SalesClass.java
@@ -0,0 +1,20 @@
+package firstdemo;
+
+public class SalesClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int[] arr = {23,45,56,78,6};
+		for(int num:arr)
+			System.out.println(num);
+		
+	}
+	public void  print() {
+		System.out.println("Print all array numbers:");
+	
+	}
+}
+
+
+
